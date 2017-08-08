c4bd075d1611c4a66b6b052be286cd4f42fb5a39
Brandon K <brandonkuhn@hotmail.com>
2017-08-07 17:54:00 -0700

    Add Nintendo Online capabilities
    Can now properly query Nintendo Online for current maps.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Callie_Bot
 A bot to show the current map roations and game modes in Splatoon 2 on Discord.
+
+#Commands
+
+Basic Commands:
+  !invite   Generates an invite link to the default channel.
+
+Moderator Commands (must have the Message Manage permission):
+  !ping     Replies with "pong". Used to check if the bot has crashed or not.
+  !clear    Clears the 100 most recent messages in the channel the command is used in.
+  !destroy  Destroys the instance of the bot. Will eventually be removed.
+
+#Requirements
+Node.js
+request-promise.js
+request-debug
--- bot.js
+++ bot.js
@@ -2,15 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const t = require('./config.js');
+const maps = require('./modules/ninAPI.js');
+
+var currentMaps = 0;
+var prevMessage = null;
 
 client.on('ready', () => {
   console.log('I am ready!');
+  client.setInterval(postCurrentMapRotation, getInterval());
 });
 
 client.on('message', message => {
   var userPerm = message.member.permissions;
   if(userPerm.has(0x00002000)){ //Has the Manage Messages permission
-    if (message.content === 'ping') {
+    if (message.content === 'ping' || message.content === "!ping") {
       message.reply('pong');
     }
     if (message.content === '!clear') {
@@ -34,7 +39,7 @@ client.on('message', message => {
       message.channel.send(result.toString());
     }, function(err) {
       console.log(err);
-      message.channel.send("Sorry something went wrong. Bug @srs_seth#9746 about this one.")
+      messageCreator(message);
     });
   }
 });
@@ -42,4 +47,61 @@ client.on('message', message => {
 function messageCreator(message){
   message.channel.send(`Something went wrong, ${message.member.guild.members.find('id', '183675563335090176')} you done goofed.`);
 }
+
+function getInterval(){
+  //2h * 60min/h * 60s/min * 1000ms/s
+  //return 2 * 60 * 60 * 1000;
+  return 6000;
+}
+
+function postCurrentMapRotation(){
+  //TODO change to off-the-hook and eventually to a generalized solution
+  var channel = client.guilds.find('name', 'Inkopolis Square').channels.find('name', 'bot-spam');
+  if(prevMessage !== null && prevMessage !== {}) prevMessage.delete();
+  if(currentMaps == 0){
+    console.log("inside if statement")
+    maps.getCurrentMaps().then(function(maps){
+      parseMapsJSON(maps);
+      return maps;
+    });
+  }else{
+    console.log("inside else statement")
+    console.log(currentMaps)
+    postNextMap();
+  }
+}
+
+function parseMapsJSON(maps){
+  var d = new Date();
+  var s = Math.floor(d.getTime()/1000);
+  //Trim off the maps that have already been rotated through, leaving only the current and upcoming maps
+  var current = 0;
+  console.log(maps.gachi[0])
+  console.log(maps.gachi[0].end_time)
+  console.log(s)
+  while(s > maps.gachi[current].end_time){
+    console.log(current, s, maps.gachi[current].end_time, s > maps.gachi[current].end_time)
+    current++;
+  }
+  maps.gachi.splice(0, current);
+  maps.regular.splice(0, current);
+  maps.league.splice(0, current);
+
+  console.log(maps)
+  // push into an array
+  // currentMaps = [];
+  // var temp = {};
+  // for(var i = 0; i < maps.gachi.length; i++){
+  //   temp = {
+  //     ranked: {
+  //       game_mode: maps.gachi[0].rule[]
+  //     }
+  //   }
+  // }
+}
+
+function postNextMap(){
+
+}
+
 client.login(t.token);
