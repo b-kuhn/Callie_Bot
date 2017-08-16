1b6d2dcfd1b9a8f05aed7720c48098f7f86100af
Brandon K <kuhn.brandon.j@gmail.com>
2017-08-15 18:17:45 -0700

    Added schedule.js
    I wanted to add this on its own branch but my git-fu is too weak.

--- modules/schedule.js
+++ modules/schedule.js
@@ -1,20 +1,55 @@
 var exports = module.exports = {};
 
 const time = require('./time.js');
+const maps = require('./maps.js');
+const f = require('./format.js')
 
-exports.scheduleMapPosts = function(client){
-  var nextPost = time.timeUntil(nextRotation) + 1 * 60 * 1000;
+var client;
+var prevMessage; //TODO figure out a better way of the bot self-moderating their messages
+var channel; //TODO change to off-the-hook and eventually to a generalized solution
+
+/**
+* 'Constructs' the schedule object. Essentially instantiates some variables that will be used throughout the module
+* @param (Discord CLient) The current instance of the Discord Client being used by the bot
+*/
+exports.constructScheduleObject = function(botClient){
+  client = botClient;
+  prevMessage = "";
+  channel = client.guilds.find('name', 'Inkopolis Square').channels.find('name', 'current-maps');
 }
 
+/**
+*
+*/
+exports.scheduleMapPosts = function(){
+  maps.current().then(function(data){
+    postMessage(f.formatMapPost(data));
+    //schedule posting
+    client.setTimeout(setAutoMapPost(), time.timeUntil(null, data.end_time + 10 * 1000));
+  });
+
+
+}
 
 /*
 * Syncs the bot up to post the new maps every two hours with the correct offset.
 */
-function setAutoMapPost(nextRotation){
-  var nextPost = time.timeUntil(nextRotation) + 1 * 60 * 1000;
-  client.setTimeout(function(){
-    postNextMap(false);
-    client.setInterval(function(){postCurrentMapRotation()},  (2 * 60 * 60 * 1000));//h * m * s * ms -- currently every two hours
-  }, nextPost);
-  console.log("Next post will occur in: ", time.getElapsedTime(nextPost));
+function setAutoMapPost(){
+  client.setInterval(postCurrentRotation,  (2 * 60 * 60 * 1000));//h * m * s * ms -- currently every two hours
+}
+
+/**
+* Posts the current rotation of stages
+*/
+function postCurrentRotation(){
+  maps.current().then(function(maps){
+
+  });
+}
+
+/**
+*
+*/
+function postMessage(post){
+  channel.send(post).then(message => prevMessage = message);
 }
